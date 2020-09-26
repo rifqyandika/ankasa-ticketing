6b20129653c462e96c6eac1059170e0858a4c48b
Mrikoarafi <mriko.arafi@gmail.com>
2020-09-26 23:43:43 +0700

    home pending

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,163 +1,19 @@
 <template>
-  <footer class="mt-5">
-    <div class="container">
-      <div class="row">
-        <div class="col-12 col-md-9  col-lg-4 ">
-          <div class="row">
-            <div class="col pl-lg-0">
-              <img src="..\..\src\assets\footer\LogoAnkasa\Group 43 (1).png" />
-            </div>
-          </div>
-          <div class="row mt-4 ">
-            <div class="col pl-lg-0">
-              <p class="linkContent">
-                Find your Flight and explore the <br />world with us. We will
-                take care of the rest
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-md col-lg-2 mb-3">
-          <div class="row mt-4 mt-md-2 mt-lg-0 choseCat">
-            <div class="col mt-lg-2 mb-lg-1 pl-lg-0">Features</div>
-          </div>
-          <div class="row linkText mt-3">
-            <div class="col-3 col-md-12 pl-lg-0  ">
-              <a href="#">Find Ticket</a>
-            </div>
-            <div
-              class="col-3 ml-sm-n5 ml-md-0 mt-md-3 col-md-12  pl-lg-0 mt-lg-3 "
-            >
-              <a href="#">My Booking</a>
-            </div>
-            <div
-              class="col-2 ml-sm-n2 ml-md-0 mt-md-3 col-md-12 pl-lg-0 mt-lg-3  "
-            >
-              <a class="#" href="#">Chat</a>
-            </div>
-            <div
-              class="col ml-sm-n3 ml-md-0 mt-md-3 col-lg-12 pl-lg-0 mt-lg-3 "
-            >
-              <a href="#">Notification</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-md-9 col-lg mb-3">
-          <div class="row choseCat mt-4 mt-lg-0">
-            <div class="col pl-lg-0 mt-lg-2">Download Angkasa app</div>
-          </div>
-          <div class="row mt-3">
-            <div class="col col-sm-5 col-md-6 col-lg-12 pl-lg-0 ">
-              <a href="#"
-                ><img
-                  src="..\..\src\assets\footer\LogoStore\apple-app-store-travel-awards-globestamp-7 3.png"
-              /></a>
-            </div>
-            <div
-              class="col col-sm-5 col-md-4 col-lg-12 text-right text-lg-left mt-lg-3 pl-lg-0"
-            >
-              <a href="#"
-                ><img
-                  src="..\..\src\assets\footer\LogoStore\apple-app-store-travel-awards-globestamp-7 2.png"
-              /></a>
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-md-3 mt-4 mb-2 mb-sm-0 mt-lg-0 col-lg">
-          <div class="row  choseCat mt-lg-2">
-            <div class="col pl-lg-0">Follow Us</div>
-          </div>
-          <div class="row  mt-3 mb-2">
-            <div class="col col-md-12 pl-lg-0">
-              <a href="#" class="mr-md-n2"
-                ><img
-                  class="ml-md-n2"
-                  src="..\..\src\assets\footer\SocialMedia\facebook.png"
-              /></a>
-              <a href="#" class="mr-md-n2"
-                ><img
-                  class="ml-4"
-                  src="..\..\src\assets\footer\SocialMedia\instagram.png"
-              /></a>
-              <a href="#" class="mr-md-n2"
-                ><img
-                  class="ml-4"
-                  src="..\..\src\assets\footer\SocialMedia\twitter.png"
-              /></a>
-              <a href="#" class="mr-md-n2"
-                ><img
-                  class="ml-4 mb-1"
-                  src="..\..\src\assets\footer\SocialMedia\youtube.png"
-              /></a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row mt-4 mt-md-3 mt-lg-5">
-        <div
-          class="iconMaps col-12 pl-lg-0 d-block d-sm-none mt-n1 text-sm-right text-lg-left"
-        >
-          <span
-            ><img
-              class="mr-1 md-md-0 mr-lg-1"
-              src="..\..\src\assets\footer\Location\map-pin.png"
-            />
-            Jakarta Indonesia
-          </span>
-        </div>
-        <div class="col mt-3 mt-sm-0 pl-lg-0 col-lg-9">
-          <p class="copyRight">© Ankasa. All Rights Reserved.</p>
-        </div>
-        <div
-          class="iconMaps col pl-lg-0 d-none d-sm-block mt-n1 text-sm-right text-lg-left"
-        >
-          <span
-            ><img
-              class="mr-1 md-md-0 mr-lg-1"
-              src="..\..\src\assets\footer\Location\map-pin.png"
-            />
-            Jakarta Indonesia
-          </span>
-        </div>
-      </div>
-    </div>
-  </footer>
+  <div class="container">
+    <Navbar />
+    <Footer />
+  </div>
 </template>
 
-<style>
-a:hover {
-  color: rgb(135, 143, 206) !important;
-}
-.linkContent {
-  font-family: Poppins;
-  font-size: 14px;
-  line-height: 221.43%;
-  color: #6b6b6b;
-}
-.linkText a {
-  font-family: Poppins;
-  font-size: 14px;
-  line-height: 21px;
-  text-decoration: none !important;
-  color: #6b6b6b;
-}
-.choseCat {
-  font-family: Poppins;
-  font-weight: 600;
-  font-size: 16px;
-  line-height: 24px;
-  color: #000000;
-}
-.copyRight {
-  font-family: Poppins;
-  font-size: 14px;
-  line-height: 21px;
-  color: #6b6b6b;
-}
-.iconMaps {
-  font-family: Poppins;
-  font-size: 14px;
+<style></style>
 
-  color: #6b6b6b;
+<script>
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+export default {
+  components: {
+    Navbar,
+    Footer
+  }
 }
-</style>
+</script>
