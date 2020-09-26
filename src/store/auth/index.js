a20b1c580d0958f70e1d96d79214aad809ec1589
wisnu345 <wisnugiri64@gmail.com>
2020-09-27 04:27:52 +0700

    login, register done

---
+++ src/store/auth/index.js
@@ -0,0 +1,69 @@
+import axios from 'axios'
+const { url } = require('../../helper/env')
+
+const state = () => {
+  return {
+    token: localStorage.getItem('token') || null
+  }
+}
+
+const getters = {
+  isLogin (state) {
+    if (state.token !== null) {
+      return true
+    } else {
+      return false
+    }
+  }
+}
+
+const actions = {
+  login (context, payload) {
+    console.log(payload)
+    return new Promise((resolve, reject) => {
+      axios.post(`${url}/user/login`, payload)
+        .then((response) => {
+          console.log(response.data)
+          if (response.data.code === 200) {
+            localStorage.setItem('token', response.data.data.token)
+            localStorage.setItem('refreshToken', response.data.data.refreshToken)
+            localStorage.setItem('id', response.data.data.id)
+          }
+          resolve(response.data)
+        })
+        .catch((err) => {
+          reject(err)
+        })
+    })
+  },
+  register (context, payload) {
+    console.log(payload)
+    return new Promise((resolve, reject) => {
+      axios.post(`${url}/user/register`, payload)
+        .then((response) => {
+          resolve(response.data)
+        })
+        .catch((err) => {
+          reject(err)
+        })
+    })
+  },
+  refreshToken (context, payload) {
+    console.log(payload)
+    return new Promise((resolve, reject) => {
+      axios.post(`${url}/users/refreshtoken`, payload)
+        .then((response) => {
+          // console.log(response)
+          localStorage.setItem('token', response.data.token)
+          resolve(response.message)
+        })
+    })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  actions,
+  getters
+}
