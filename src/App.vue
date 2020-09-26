95cd76a2d0a1a7bb4cf8fa8b9903c597ba5b76ab
Mrikoarafi <mriko.arafi@gmail.com>
2020-09-26 23:09:49 +0700

    footer done

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
-#app{
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap");
+
+#app {
   margin: 0;
   padding: 0;
 }
